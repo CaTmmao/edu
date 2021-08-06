f59d84492ad9a65348f9b1a7c2e094d2169d55d2
catmmao <catmmao@gmail.com>
2021-08-06 12:20:52 +0800

    refactor: 入口文件类名修改

--- edu-teacher/src/main/java/com/catmmao/edu/EduTeacherApplication.java
+++ edu-teacher/src/main/java/com/catmmao/edu/EduTeacherApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EduApplication {
+public class EduTeacherApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EduApplication.class, args);
+        SpringApplication.run(EduTeacherApplication.class, args);
         System.out.println("ok");
     }
 }
