eeb58664ed1ec62fc195ce69a585b28b1b16c04b
catmmao <catmmao@gmail.com>
2021-08-09 15:11:56 +0800

    chore(teacher模块): 解决跨域

--- edu-teacher/src/main/java/com/catmmao/edu/controller/EduTeacherController.java
+++ edu-teacher/src/main/java/com/catmmao/edu/controller/EduTeacherController.java
@@ -14,6 +14,7 @@ import com.catmmao.edu.service.EduTeacherService;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/teacher")
+@CrossOrigin
 public class EduTeacherController {
     @Resource
     private EduTeacherService eduTeacherService;
