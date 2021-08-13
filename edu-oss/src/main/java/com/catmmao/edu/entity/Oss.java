4b006f6cde5768737bb614b12aaeca2b99d66a05
catmmao <catmmao@gmail.com>
2021-08-13 23:32:59 +0800

    fix(oss模块): oss参数填写错误

--- edu-oss/src/main/java/com/catmmao/edu/entity/Oss.java
+++ edu-oss/src/main/java/com/catmmao/edu/entity/Oss.java
@@ -2,7 +2,7 @@ package com.catmmao.edu.entity;
 
 //oss 配置
 public class Oss {
-    public static final String ENDPOINT = "http://oss-cn-beijing.aliyuncs.com";
+    public static final String ENDPOINT = "oss-cn-beijing.aliyuncs.com";
     public static final String ACCESSKEYID = "LTAI5tGKoPEDU4TGWiYQSobn";
     public static final String ACCESSKEYSECRET = "2Azul3juFQidmOR6fljsuqKIsLXqYG";
     public static final String BUCKETNAME = "edu-site";
